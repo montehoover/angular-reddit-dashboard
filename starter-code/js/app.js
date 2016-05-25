f74f561c019095cd8760985e8d5bc1ec9d704ef7
montehoover <monte.b.hoover@gmail.com>
2016-05-24 17:37:30 -0700

    Displayed results, images, and history.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,35 @@
-/* put js code here */
+var redditApp = angular.module("RedditApp", []);
+
+redditApp.controller("HomeCtrl", ['$scope', '$http', function($scope, $http) {
+
+  $scope.searchTerm = ''
+
+  $scope.history = []
+
+  $scope.results = {}
+
+  $scope.search = function() {
+    $scope.history.push($scope.searchTerm)
+
+    var req = {
+      url: "http://www.reddit.com/search.json",
+      method: "GET",
+      params: {
+        q: $scope.searchTerm
+      }
+    }
+    
+    $http(req).then(function success(res) {
+      var redditData = res.data;
+      $scope.results = redditData.data.children;
+      console.log(redditData.data.children)
+    }, function error(res) {
+      console.log(res)
+    })
+  }
+
+
+
+}])
 
 console.log('this file is running.');
